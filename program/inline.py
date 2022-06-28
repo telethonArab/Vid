78f04f3e47dfa7090b98d2d5da50529c41d75fa9
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-06-28 19:23:21 +0300

    Update inline.py

--- program/inline.py
+++ program/inline.py
@@ -16,7 +16,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="type a youtube video name...",
+            switch_pm_text=" جاري البحث ...",
             switch_pm_parameter="help",
             cache_time=0,
         )
@@ -43,6 +43,6 @@ async def inline(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="error: search timed out",
+                switch_pm_text="خطا في البحث",
                 switch_pm_parameter="",
             )
